78f4d7129880760b70a0cd1cf38b9eff2a283b7d
Hasnain11222 <109806090+Hasnain11222@users.noreply.github.com>
2022-08-29 20:48:15 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,80 @@
+// normal function
+
+// function sum(){
+//     console.log(10+50)
+// }
+
+// sum()
+
+// function multiply(){
+//     console.log(30*40)
+// }
+// multiply()
+
+
+// function minus(){
+//     console.log(30-50)
+
+// }
+
+// minus()
+
+// function divid(){
+//     console.log(40/7)
+// }
+
+
+
+// parmeter function
+
+// divid()
+
+
+
+
+function sum(a,b){
+    console.log(a+b);
+}
+
+sum(100,300)
+
+
+
+
+
+function multiply(a,b){
+    console.log(a*b);
+}
+
+multiply(100,30)
+
+
+
+function divide(a,b){
+    console.log(a/b);
+}
+
+divide(200,300)
+
+
+
+function minus(a,b){
+    console.log(a-b);
+}
+minus(2000,500)
+
+
+
+
+
+// get percentage
+
+
+function percentage(a,b,c){
+    console.log(a/b*c);
+}
+
+
+percentage(380,500,100)
+
+
---
+++ app2.js
@@ -0,0 +1,39 @@
+let sum = () =>{
+    console.log(10+20);
+}
+
+sum()
+
+
+
+let multiply = () =>{
+    console.log(10*20);
+}
+
+multiply()
+
+
+
+
+let minus = () =>{
+    console.log(10-20);
+}
+
+minus()
+
+
+
+let divid = () =>{
+    console.log(10/20);
+}
+
+divid()
+
+
+
+
+let percentage = () =>{
+    console.log(380/500*100);
+}
+
+percentage()
